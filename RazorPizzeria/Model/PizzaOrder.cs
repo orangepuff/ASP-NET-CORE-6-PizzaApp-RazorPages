911b70f141081086827e7f2a503a72a9d201158c
Saranya Kaewpradit <saranya.kaewpradit@pcmicorp.com>
2024-01-17 14:23:01 +0700

    clear

--- RazorPizzeria/Model/PizzaOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RazorPizzeria.Model
-{
-    public class PizzaOrder
-    {
-        public int Id { get; set; }
-        public string PizzaName { get; set; }
-        public float BasePrice { get; set; }
-    }
-}
